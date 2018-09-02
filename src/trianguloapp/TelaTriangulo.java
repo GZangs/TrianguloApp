d2e9930b7b054ed931959a257495e6600fd1d828
Gabriel <zanghelinigab@gmail.com>
2018-09-02 17:05:18 -0300

    Criada classe Triangulo que contém o código de negócio

--- src/trianguloapp/TelaTriangulo.java
+++ src/trianguloapp/TelaTriangulo.java
@@ -13,55 +13,22 @@ import java.util.Set;
 public class TelaTriangulo {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int terst=0;
+        Triangulo triangulo1 = new Triangulo();
+        
         System.out.println("Informa o tipo do triangulo");
 
         System.out.println("Informa o valor do lado a");
-        double a = sc.nextDouble();
+        triangulo1.setA(sc.nextDouble());
 
         System.out.println("Informa o valor do lado b");
-        double b = sc.nextDouble();
+        triangulo1.setB(sc.nextDouble());
 
         System.out.println("Informa o valor do lado c");
-        double c = sc.nextDouble();
-
-        Set<String> propriedades = new HashSet<String>();
-        double hipotenusa = a;
-
-        if (hipotenusa < b) {
-            hipotenusa = b;
-        }
-
-        if (hipotenusa < c) {
-            hipotenusa = c;
-        }
-
-        if ((a + b + c - hipotenusa) > hipotenusa) {
-            propriedades.add("Válido");
-
-            if (a == b || b == c || c == a) {
-                if (a == b && b == c) {
-                    propriedades.add("Equilátero");
-                } else {
-                    propriedades.add("Isósceles");
-                }
-            }
-
-            if (a != b && b != c && c != a) {
-                propriedades.add("Escaleno");
-            }
-
-            if (((a * a + b * b + c * c) - hipotenusa * hipotenusa) == hipotenusa
-                    * hipotenusa) {
-                propriedades.add("Retângulo");
-            }
-        } else {
-            propriedades.add("Inválido");
-        }
+        triangulo1.setC(sc.nextDouble());
 
         System.out.println("Propriedades do triangulo ->");
 
-        for (String valor : propriedades) {
+        for (String valor : triangulo1.GetProperties()) {
             System.out.println(valor);
         }
     }
